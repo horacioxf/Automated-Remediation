e61eaa3436fe386b1f560183574439b8ba430aca
Horacio Flores <100793672+horacioxf@users.noreply.github.com>
2025-06-01 23:34:57 -0500

    Update remediation-root-password.sh

--- scripts/remediation-root-password.sh
+++ scripts/remediation-root-password.sh
@@ -6,7 +6,7 @@ echo -e "Cyberlab123!\nCyberlab123!" | sudo passwd root
 rm remediation-root-password.sh
 
 # Download the script
-# wget https://raw.githubusercontent.com/joshmadakor1/lognpacific-public/main/automation/remediation-root-password.sh --no-check-certificate
+# wget https://raw.githubusercontent.com/horacioxf/Automated-Remediation/refs/heads/main/scripts/remediation-root-password.sh --no-check-certificate
 
 # Make the script executable:
 # chmod +x remediation-root-password.sh
